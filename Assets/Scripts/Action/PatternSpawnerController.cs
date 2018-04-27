81477036a58308a2862e91536d8c736fa0aa7f31
Giovanni Barrottu <johnny06@live.com.au>
2018-04-27 15:37:28 +1000

    Initial commit

---
+++ Assets/Scripts/Action/PatternSpawnerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternSpawnerController : MonoBehaviour
+{
+    public IntVariable enemyCount;
+
+
+    public void SpawnPattern(GameObject chosenPattern)
+    {
+        StartCoroutine(SpawnPatternIE(chosenPattern));
+    }
+    public IEnumerator SpawnPatternIE(GameObject pattern)
+    {
+        GameObject patternInstance = Instantiate(pattern, transform);
+
+        yield return new WaitForSeconds(1);
+
+        for (int i = 0; i < 50000; i++)
+        {
+            if (enemyCount.value == 0)
+            {
+                yield return new WaitForSeconds(1.9f);
+                Destroy(patternInstance);
+                break;
+            }
+            yield return new WaitForSeconds(0.05f);
+        }
+
+    }
+}
---
+++ Assets/TitleFadeOut.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleFadeOut : MonoBehaviour {
+
+    bool fade;
+
+	// Use this for initialization
+	void Start () {
+        fade = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            fade = true;
+        }
+
+        if (fade)
+        {
+            //GetComponent<TextMesh>().color = Vector4.Lerp(GetComponent<TextMesh>().color, Color.clear, 0.1f);
+            gameObject.transform.position += Vector3.up * 30;
+        }
+	}
+}
